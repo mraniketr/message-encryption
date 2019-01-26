c8b8453c6d64215f070db552b6dc524196b803e4
ANIKET RAI <prosniperaniket@gmail.com>
2019-01-26 21:13:25 +0530

    added base64 encoding

--- crypto.py
+++ crypto.py
@@ -1,3 +1,6 @@
+import pyperclip
+import base64
+
 # reverse cipher
 def revc(msg):
     crev=''
@@ -7,13 +10,41 @@ def revc(msg):
         i=i-1
     return  crev
         
+
+#transposition cipher
+#ref - https://inventwithpython.com/hacking/chapter8.html
+def main(msg1):
+    msg=msg1
+    print("Enter Key")
+    key=int(input())
+    cipher=encmsg(key,msg)
+    print(cipher + '|')
+    pyperclip.copy(cipher)
+    
+def encmsg(mkey,message):
+    cipher=['']*mkey
+    for col in range(mkey):
+        pos=col
+        while pos<len(message):
+            cipher[col]+=message[pos]
+            pos+=mkey
+    return ''.join(cipher)
+
+        
 print ("input message")
 msg=input()
-print("1. Rev cipher \n 2. ROT13")
+print("1.Rev cipher \n2.Transposition Cipher \n3.base64 Cipher")
 ch = int(input())
 if ch==1:
     print(revc(msg))
 elif ch==2:
-    print(1)
+    if __name__=='__main__':
+        main(msg)    
+elif ch==3: 
+    #base64 encoding3 
+    #https://docs.python.org/2/library/base64.html
+    encoded_data = base64.b64encode('Encode this text')
+    print("Encoded text with base 64 is")
+    print(encoded_data)
 else:
     print(1)
