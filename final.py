105fb1d4439f8d6630c263460835a667b5f71d9c
Aniket-PC <prosniperaniket@gmail.com>
2019-03-23 18:34:13 +0530

    minor fixes

--- final.py
+++ final.py
@@ -2,35 +2,41 @@ from files.reversecipher import *
 from files.transpostion import *
 from files.basesix4 import *
 
-print ("input message")
-msg=input()
-print("1.Rev cipher \n2.Transposition Cipher \n3.base64 Cipher")
+print("Encoding")
+print("\n1.Rev cipher \n2.Transposition Cipher \n3.base64 Cipher")
 ch = int(input())
-if ch==1:
+
+print("Input message")
+msg = input()
+
+if ch == 1:
     print(revc(msg))
-elif ch==2:
+elif ch == 2:
         tc(msg)
-elif ch==3:
+elif ch == 3:
     print(str(enc(msg),'utf-8'))
     pyperclip.copy(str(enc(msg),'utf-8'))
 else:
-    print(1)
+    print("Incorrect Input")
 
 
-print ("\n\nDecoding\n")
+print ("\nDecoding\n")
 
 print("1.Rev cipher \n2.Transposition Cipher \n3.base64 Cipher")
+
 print("Enter your choice")
 ch=int(input())
-if ch ==1:
-    emsg = input()
+
+print("Enter Cipher")
+emsg = input()
+
+if ch == 1:
     print(revc(emsg))
 
-elif ch ==2:
-    emsg=input()
+elif ch == 2:
     dc(emsg)
 
-elif ch==3:
-    encoded_data = input()
-    print(str(dec(encoded_data), 'utf-8'))
-
+elif ch == 3:
+    print(str(dec(emsg), 'utf-8'))
+else:
+    print("Incorrect Input")
